c6ccfbcc8de509412beb45cadda282575ec09b4d
Zahyr Seferina <zahyr.seferina@gmail.com>
2019-05-14 18:36:52 +0200

    Updates

---
+++ precache-manifest.1d7f808d6b65bf52b5af7fe4361a5637.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4db189595e549b9a333445cacc0d2191",
+    "url": "/lox.spaces/index.html"
+  },
+  {
+    "revision": "b68582ed7155c8a23a00",
+    "url": "/lox.spaces/static/css/main.3142ae65.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/lox.spaces/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "b68582ed7155c8a23a00",
+    "url": "/lox.spaces/static/js/main.626918f4.chunk.js"
+  },
+  {
+    "revision": "184209c0dbdea3c839bd",
+    "url": "/lox.spaces/static/js/runtime~main.613bc991.js"
+  }
+]);
